0e10198a7d18e7846a032a73aca93446cef6a072
spectrecode <mchristy.ccb@gmail.com>
2016-09-14 13:38:40 -0500

    añadiendo traduccion

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("form-signin-heading").innerHTML =
+"Por favor inicia sesión";
+document.getElementById("inputEmail").placeholder = "Correo Electrónico";
+document.getElementById("inputPassword").placeholder = "Contraseña";
+document.querySelector("span").innerHTML = "Recordar Datos";
+document.querySelector("button").innerHTML = "Iniciar sesión";
